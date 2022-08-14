fc7c9c9abc067912c11ba6a48c9c7a05a209b722
piotr czerski <piotr.czerski10@gmail.com>
2022-08-14 15:00:50 +0200

    Custom EmailException

---
+++ BookstoreManagment.API/BookstoreManagment.Domain/Exceptions/EmailException.cs
@@ -0,0 +1,9 @@
+﻿namespace BookstoreManagement.Domain.Exceptions;
+
+public class EmailException : Exception
+{
+    public EmailException(string email, Exception ex ) : base($"Email \"{email}\" is invalid.", ex)
+    {
+        throw new NotImplementedException();
+    }
+}
