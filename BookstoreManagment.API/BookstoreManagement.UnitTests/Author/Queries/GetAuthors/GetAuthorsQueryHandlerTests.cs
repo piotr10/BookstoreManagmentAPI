c6d87acd6027eddf2f067316762754ca84f411be
Piotr Czerski <piotr.czerski10@gmail.com>
2022-09-20 22:42:41 +0200

    Added first TDD tests GetQueryHandlerTests for Authors, Books, Customers and Orders

---
+++ BookstoreManagment.API/BookstoreManagement.UnitTests/Author/Queries/GetAuthors/GetAuthorsQueryHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BookstoreManagement.Application.Author.Queries.GetAllAuthors;
+using BookstoreManagement.Persistance;
+using BookstoreManagement.UnitTests.Common;
+using Shouldly;
+
+namespace BookstoreManagement.UnitTests.Author.Queries.GetAuthors;
+
+[Collection("QueryCollection")]
+public class GetAuthorsQueryHandlerTests
+{
+    private readonly BookstoreDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAuthorsQueryHandlerTests(QueryTestFixtures fixtures)
+    {
+        _context = fixtures.Context;
+        _mapper = fixtures.Mapper;
+    }
+
+    [Fact]
+    public async Task CanGetAuthors()
+    {
+        var handler = new GetAuthorsQueryHandler(_context, _mapper);
+        var result = await handler.Handle(new GetAuthorsQuery(), CancellationToken.None);
+
+        result.ShouldBeOfType<AuthorsVm>();
+        result.Authors.Count.ShouldBe(3);
+        result.Authors.ShouldNotBeNull();
+    }
+}
---
+++ BookstoreManagment.API/BookstoreManagement.UnitTests/Order/Queries/GetOrders/GetOrdersQueryHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BookstoreManagement.Application.Order.Queries.GetAllOrders;
+using BookstoreManagement.Persistance;
+using BookstoreManagement.UnitTests.Common;
+using Shouldly;
+
+namespace BookstoreManagement.UnitTests.Order.Queries.GetOrders;
+
+[Collection("QueryCollection")]
+public class GetOrdersQueryHandlerTests
+{
+    private readonly BookstoreDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetOrdersQueryHandlerTests(QueryTestFixtures fixtures)
+    {
+        _context = fixtures.Context;
+        _mapper = fixtures.Mapper;
+    }
+
+    [Fact]
+    public async Task CanGetOrders()
+    {
+        var handler = new GetOrdersQueryHandler(_context, _mapper);
+        var result = await handler.Handle(new GetOrdersQuery(), CancellationToken.None);
+
+        result.ShouldBeOfType<OrdersVm>();
+        result.Orders.Count.ShouldBe(3);
+    }
+}
