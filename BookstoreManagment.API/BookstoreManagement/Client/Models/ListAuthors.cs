062b02eb83d57330626604bf602496955a6eda8a
Piotr Czerski <piotr.czerski10@gmail.com>
2022-09-22 23:37:30 +0200

    Added Table Author page + Add new Author page + 2 new models: AuthorDto, AuthorsForm

--- BookstoreManagment.API/BookstoreManagement/Client/Models/ListAuthors.cs
+++
@@ -1,8 +0,0 @@
-﻿using BookstoreManagement.Application.Author.Queries.GetAllAuthors;
-
-namespace BookstoreManagement.Client.Models;
-
-public class ListAuthors
-{
-    public ICollection<AuthorDto> Authors { get; set; }
-}
